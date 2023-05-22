3b8c8c1f71999a9dee32cdd86230611009a166cc
AlexGuseinov <ali.guseynof2503@gmail.com>
2023-05-22 21:55:44 +0400

    fixed -> unique username & email, small changes

--- src/main/java/az/code/travelTechdemo/services/UserServiceimpl.java
+++ src/main/java/az/code/travelTechdemo/services/UserServiceimpl.java
@@ -4,6 +4,7 @@ import az.code.travelTechdemo.models.entities.User;
 import az.code.travelTechdemo.repository.UserRepository;
 import az.code.travelTechdemo.services.impl.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,7 +17,17 @@ public class UserServiceimpl implements UserService {
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
-    public User findByUserName(String userName){
-        return  userRepository.findAllByUsername(userName).orElseThrow();
+
+    public User getUserByEmail(String email) {
+        return userRepository.findAllByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException("Couldn't found the user!!")
+        );
     }
+
+    public User saveUser(User user) {
+        if (userRepository.findAllByEmail(user.getEmail()).isEmpty())
+            userRepository.save(user);
+        return user;
+    }
+
 }
